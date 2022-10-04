d59aaede7eee7fe8d2b1a155ee4ac42cded1d178
uch2ha <sinyavskiy.d@gmail.com>
2022-10-04 13:01:05 +0300

    creating btn

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import { useEffect } from 'react';
 import './App.css';
-const tg = window.Telegram.WebApp;
 
 function App() {
     useEffect(() => {
         tg.ready();
     }, []);
 
-    const onClose = () => {
-        tg.close();
-    };
     return (
         <div className='App'>
             <p>working</p>
-            <button onClick={onClose}>Close</button>
         </div>
     );
 }
